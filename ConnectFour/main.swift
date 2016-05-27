bfdfb7c08d854110cb21d024b51eeb9c71562b65
jipp <wolfgang.keller@wobilix.de>
2016-05-27 23:27:56 +0200

    Initial Commit

---
+++ ConnectFour/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ConnectFour
+//
+//  Created by Wolfgang Keller on 27.05.16.
+//  Copyright © 2016 Wolfgang Keller. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
